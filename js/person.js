4864c4222e0ede3e3772785b216b49214600f5d3
icreeative <matyszkowicz.mateusz@gmail.com>
2017-01-04 16:59:35 +0100

    Update person.js

--- js/person.js
+++ js/person.js
@@ -1,61 +1,37 @@
-//author: Mateusz Matyszkowicz
-//project: Person tasks
-
-(function (){
-	var Person = function(){};
-	
-	Person.prototype.task = [];
-
-	//Funkca dokładająca na koniec tablicy task[] nowe obiekty.
-	Person.prototype.addTask = function(name, status){
-		var task = { 'name' : name, 'completed' : false | status};
-		this.task.push(task);
-	};
+var Person = (function(){
+    var tasks = [
+        { name: "Compl1", complete: false },
+        { name: "Compl2", complete: false },
+        { name: "Compl3", complete: false },
+        { name: "Compl4", complete: true },
+        { name: "Compl5", complete: true },
+        { name: "Compl6", complete: true }
+    ];
 
 	//Funkcja usuwająca i zwracająca ostatni task z listy.
-	Person.prototype.removeTask = function(){
-		 return this.task.pop();
+	removeTask = function( id){
+         return tasks.splice(id,1);
 	};
-	//Funkcjia umozliwaiająca zmiane statusu dla task
-	Person.prototype.changeTaskStatus = function( taskID, status ){
-		this.task[taskID].completed = status;
-	}
-
-	Person.prototype.displayTask = function (task){
-		console.log(task.name + " is " + (task.completed ? 'completed' : 'unfinished'));
-	}
-
-	//Wysiwtla wszystkie obiekty z tablicy task.
-	Person.prototype.showAllTask = function(){
-		this.task.forEach(this.displayTask);
-	}
 
 	//Wypisuje zadania w zależności od wybranego statusu 0 - unfinished, 1 - completed
-	Person.prototype.showTasks = function ( status ){
-		if(status)
-			this.task.filter(isCompleted).forEach(this.displayTask);
-		else
-			this.task.filter(isUnFinished).forEach(this.displayTask);
+	getFinishedTasks = function (){
+		return tasks.filter(task => task.complete)
 	}
 
-	//Filtry dla Array.protoype.filter
-	isCompleted = function(element){
-		return  (element.completed == true);
-	}
+    getAllTasks = function(){
+        return tasks;
+    }
 
-	isUnFinished = function(element){
-		return (element.completed == false);
-	}
-
-
+    return{
+        finishedTasks: getFinishedTasks,
+        removeTask: removeTask,
+        allTasks: getAllTasks
+    }
 	//Testowanie 
-	var Mat = new Person();
-	Mat.addTask("Task1", false);
-	Mat.addTask("Task2", false);
-	Mat.addTask("Task3", false);
-	Mat.addTask("Task4", true);
-	Mat.addTask("Task5", false);
-	Mat.addTask("Task6", true);
-	Mat.changeTaskStatus(0, 1);
-	Mat.showTasks(true);
-})()
+})();
+
+var P = Person;
+P.removeTask(0);
+console.log(P.finishedTasks());
+
+//console.log(P.allTasks());
